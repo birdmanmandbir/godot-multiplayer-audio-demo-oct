317bc99ef0f2b081568598d1bdb72a78da88b1e7
neilbb <bn0010100@gmail.com>
2024-10-31 08:16:05 -0500

    [twovoip] try to migrate

--- audio_manager.gd
+++ audio_manager.gd
@@ -2,11 +2,13 @@ extends Node
 
 @onready var input: AudioStreamPlayer
 var index: int
-var effect: AudioEffectCapture
-var playback: AudioStreamGeneratorPlayback
+var effect: AudioEffectOpusChunked
 @export var outputPath: NodePath
 var inputThreshold: float = 0.005
-var receiveBuffer := PackedFloat32Array()
+var receiveBuffer := []
+
+var outputstreamopuschunked: AudioStreamOpusChunked
+var prefixbyteslength = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,44 +27,39 @@ func setupAudio(id):
 		effect = AudioServer.get_bus_effect(index, 0)
 	
 	# remember to enable autoplay for the output
-	playback = get_node(outputPath).get_stream_playback()
+	outputstreamopuschunked = get_node(outputPath).get_stream()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if is_multiplayer_authority():
+	if is_multiplayer_authority() and multiplayer.multiplayer_peer and multiplayer.multiplayer_peer.get_connection_status() != MultiplayerPeer.CONNECTION_CONNECTED:
 		processMic()
 	processVoice()
 	pass
 
 func processMic():
-	var sterioData: PackedVector2Array = effect.get_buffer(effect.get_frames_available())
-
-	if sterioData.size() > 0:
-		var data = PackedFloat32Array()
-		data.resize(sterioData.size())
+	var prepend = PackedByteArray()
+	while effect.chunk_available():
+		var opusdata: PackedByteArray = effect.read_opus_packet(prepend)
+		effect.drop_chunk()
 		var maxAmplitude := 0.0
 
-		for i in range(sterioData.size()):
-			# average the two channels
-			var value = (sterioData[i].x + sterioData[i].y) / 2.0
-			maxAmplitude = max(maxAmplitude, value)
-			data[i] = value
+		for i in range(opusdata.size()):
+			# loud enough then send to server
+			maxAmplitude = max(maxAmplitude, opusdata[i])
 		
-		# loud enough then send to server
 		if maxAmplitude < inputThreshold:
-			return
+			continue
 		
-		sendData.rpc(data)
+		sendData.rpc(opusdata)
 
 func processVoice():
-	if receiveBuffer.size() <= 0:
+	if receiveBuffer.size() == 0:
 		return
 
-	for i in range(min(playback.get_frames_available(), receiveBuffer.size())):
-		# input don't have left or right channel, so we use the same value for both
-		playback.push_frame(Vector2(receiveBuffer[0], receiveBuffer[0]))
-		receiveBuffer.remove_at(0)
+	while outputstreamopuschunked.chunk_space_available():
+		var fec = 0
+		outputstreamopuschunked.push_opus_packet(receiveBuffer, prefixbyteslength, fec)
 
 @rpc("any_peer", "call_remote", "unreliable_ordered")
-func sendData(data: PackedFloat32Array):
+func sendData(data: PackedByteArray):
 	receiveBuffer.append_array(data)
